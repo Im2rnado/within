1dbba2690be8a7fc187630efad87a133fd01dd28
Im2rnado <yassin.bedier@gmail.com>
2024-11-14 19:53:14 +0200

    zabat login with api

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,17 +1,35 @@
 import { Container, Col, Row } from "react-bootstrap";
+import { useState } from 'react';
 import Register from "./components/register";
 import Login from "./components/login";
+import axios from "axios";
+
 function App() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const login = async () => {
+    try {
+      const res = await axios.post("http://localhost:4000/login", { username, password });
+
+      if (res.data.success === true) {
+        alert(res.data.message);
+      } else {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  }
+
   return (
     <Container>
       <Row>
-        {/* Two columns in large and medium devices and one column on small and extra small devices */}
-        <Col xs={12} sm={12} md={6} lg={6}>
-          <Register />
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6}>
-          <Login />
-        </Col>
+        {
+          <Col xs={12} sm={12} md={6} lg={6}>
+            <Login setUsername={setUsername} setPassword={setPassword} login={login} />
+          </Col>
+        }
       </Row>
     </Container>
   );
--- client/client/src/components/login.js
+++ client/client/src/components/login.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
-export default function Login() {
+
+export default function Login({ setUsername, setPassword, login }) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        login();
+    }
+
     return (
         <>
             <h2>Login</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 {/* username */}
                 <Form.Group controlId="formBasicUsername">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="username" placeholder="Enter username" />
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </Form.Group>
                 {/* password */}
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
                 {/* submit button */}
                 <Button variant="primary" type="submit">
@@ -22,4 +36,4 @@ export default function Login() {
             </Form>
         </>
     )
-} 
+}
