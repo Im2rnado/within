04f8faf9d7fa1b2b0051e5d7b73b367c1252a30e
BEB024 <ilabib2004@gmail.com>
2024-11-14 16:21:39 +0200

    changed port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const port = 8080;
+const port = 4000;
 const router = express.Router();
 const app = express();
 const cors = require("cors");
@@ -12,17 +12,21 @@ app.use(cors());
 app.use("/", router);
 
 router.get("/", (req, res) => {
-    res.sendFile(__dirname + "/login.html");
+  res.sendFile(__dirname + "/login.html");
 });
 
 router.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    console.log("Username: " + username + ", Password: " + password);
-    res.send("<h1>Welcome " + username + "!</h1> <h2>You have successfully logged in.</h2>");
-})
+  console.log("Username: " + username + ", Password: " + password);
+  res.send(
+    "<h1>Welcome " +
+      username +
+      "!</h1> <h2>You have successfully logged in.</h2>"
+  );
+});
 
 app.listen(port, () => {
-    console.log("Server is running on http://localhost:" + port);
+  console.log("Server is running on http://localhost:" + port);
 });
