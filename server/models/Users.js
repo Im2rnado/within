62cf8733031fa6b6c073446e193f5f6dd326d9d0
sezm00 <islam.abdelmotaleb23d@eslsca.edu.eg>
2024-11-17 03:12:00 +0200

    added to userSchema profilePicture

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    type: { type: String, enum: ["student", "admin"], default: "student" }
+    type: { type: String, enum: ["student", "admin"], default: "student" },
+    profilePicture: {type: String} /*url to image*/
 });
 
 module.exports = mongoose.model("Users", userSchema);
