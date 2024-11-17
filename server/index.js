584eb2adbdbdc136cd1213836865669581154a45
sezm00 <islam.abdelmotaleb23d@eslsca.edu.eg>
2024-11-17 20:19:52 +0200

    backend for forgetPassword Form

--- server/index.js
+++ server/index.js
@@ -81,6 +81,43 @@ router.post("/register", async (req, res) => {
     }
 })
 
+router.post("/forgetPassword", async(req, res)=>{
+    console.log("[POST] /forgetPassword");
+    
+    let username = req.body.username;
+    const currentPassword = req.body.password;
+    const newPassword = req.body.newPassword;
+
+    username = username.toLowerCase();
+
+    try{
+        const user = await Users.findOne({ $or: [{email} , {username}] });
+        
+        if (!user){
+            console.log("[400] User not found");
+            res.status(400).json({succes: false, message:"Incorrect Username"});
+        }
+        const checkPassword = await bcrypt.compare(currentPassword, user.password);
+        
+        if (!checkPassword){
+            console.log("[400] Incorrect Password");
+            return res.status(400).json({ success: false, message: "Incorrect password" });
+        }
+
+        const currentPassword = newPassword;
+        const hashedPassword = await bcrypt.hash(currentPassword, 10)
+        //ertegal men hena moot
+        const updateUser = await Users.updateOne({password: hashedPassword});
+        await updateUser.save();
+
+        console.log("[200] User created successfully");
+        return res.status(200).json({ success: true, message: "User created successfully" });
+    } catch (error) {
+        console.log("[500] Internal server error");
+        res.status(500).json({ success: false, message: error.message });
+    }    
+})
+
 app.listen(port, () => {
     console.log("[SERVER] Running on http://localhost:" + port);
 });
