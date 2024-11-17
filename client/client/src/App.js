57dd819bafb99b69153d20dd1e99285ff6ffc06a
sezm00 <islam.abdelmotaleb23d@eslsca.edu.eg>
2024-11-17 20:30:29 +0200

    deleted unneeded

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Container} from "react-bootstrap";
-=======
-import { Container } from "react-bootstrap";
->>>>>>> 0c5359912aeed90b6fa1512f14b3b23e28209178
 import { useState } from 'react';
 import Register from "./components/register";
 import ForgetPassword from "./components/forgetPassword"
