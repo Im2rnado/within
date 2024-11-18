5cc915a37ef3a185ef2df0f11f1aad428cda7a16
BEB024 <ilabib2004@gmail.com>
2024-11-18 20:15:21 +0200

    Changed header to Header in App.js

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,15 +1,17 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import Sidebar from "./components/sidebar";
 import Boxes from "./components/boxes";
-import "./css/App.css"
+import "./css/App.css";
 
 function App() {
   return (
     <div className="main-container">
       <Sidebar />
       <div className="middle-container">
-        <div><Header /></div>
-        <div style={{ marginTop: '20vh', marginLeft: '35vh' }}>
+        <div>
+          <Header />
+        </div>
+        <div style={{ marginTop: "20vh", marginLeft: "35vh" }}>
           {
             // Add your components here
             <Boxes />
